6549c5bbfda39829ace70d4191069a5537962338
woong9149 <sewoong0409@naver.com>
2022-05-11 23:09:08 +0900

    20220511

---
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,45 @@
+package hello.servlet.basic;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    /**
+     * @WebServlet : 서블릿 애노테이션
+     * name : 서블릿 이름
+     * urlPatterns : URL 맵핑
+     *
+     * HttpServletRequest 역할
+     *  - HTTP 요청 메시지를 편리하게 조회할 수 있다.
+     *  - 부가 기능
+     *      - 임시 저장소 기능
+     *          - 해당 HTTP 요청이 시작부터 끝날 때 까지 유지되는 임시 저장소 기능
+     *              - 저장: request.setAttribute(name, value)
+     *              - 조회: request.getAttribute(name)
+     *      - 세션 관리 기능
+     *          - request.getSession(create: true)
+     *  - HttpServletRequest, HttpServletResponse를 사용할 때 가장 중요한 점은 이 객체들이 HTTP 요청 메세지와 응답 메세지를
+     *    편리하게 사용하도록 도와주는 객체라는 점이다. 따라서 이기능에 대해서 깊이 있는 이해를 하려면, HTTP 스펙이 제공하는 요청, 응답 메세지 자체를
+     *    이해해야 한다.
+     */
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write("hello " + username);
+
+    }
+}
