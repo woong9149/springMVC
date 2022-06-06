e64e2479fc8ad678adea7bd141e59375d46d6d7a
woong9149 <sewoong0409@naver.com>
2022-06-06 21:32:34 +0900

    20220606

--- src/main/java/hello/servlet/web/frontController/v5/FrontControllerServletV5.java
+++ src/main/java/hello/servlet/web/frontController/v5/FrontControllerServletV5.java
@@ -46,6 +46,20 @@ import java.util.Map;
  *  v5: 유연한 컨트롤러
  *  - 어댑터 도입
  *  - 어댑터를 추가해서 프레임워크를 유연하고 확장성 있게 설계
+ *
+ *  SpringMVC 구조
+ *  - 동작 순서 -
+ *  1. 핸들러 조회: 핸들러 매핑을 통해 요청 URL에 매핑된 핸들러(컨트롤러)를 조회한다.
+ *  2. 핸들러 어댑터 조회: 핸들러를 실행할 수 있는 핸들러 어댑터를 조회한다.
+ *  3. 핸들러 어댑터 실행: 핸들러 어댑터를 실행한다.
+ *  4. 핸들러 실행: 핸들러 어댑터가 실제 핸들러를 실행한다.
+ *  5. ModelAndView 반환: 핸들러 어댑터는 핸들러가 반환하는 정보를 ModelAndView로 변환해서 반환한다.
+ *  6. ViewResolver 호출: viewResolver를 찾고 실행한다.
+ *      - JSP의 경우: InternalResourceViewResolver가 자동 등록되고, 사용된다.
+ *  7. view 반환: viewResolver는 뷰의 논리 이름을 물리 이름으로 바꾸고, 렌더링 역할을 하는 뷰 객체를 반환한다.
+ *      - JSP의 경우: InternalResourceView(JstlView) 를 반환하는데, 내부에 forward() 로직이 있다.
+ *  8. 뷰 렌더링: 뷰를 통해서 뷰를 렌더링 한다.
+ *
  */
 @WebServlet(name = "frontControllerServletV5", urlPatterns = "/front-controller/v5/*")
 public class FrontControllerServletV5 extends HttpServlet {
